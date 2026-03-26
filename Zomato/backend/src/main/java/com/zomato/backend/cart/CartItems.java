a02093789273ae33ae5623051873a23e169ab3c5
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2026-03-26 22:57:38 +0530

    chore: phase 3 progress

--- Zomato/backend/src/main/java/com/zomato/backend/cart/CartItems.java
+++ Zomato/backend/src/main/java/com/zomato/backend/cart/CartItems.java
@@ -1,3 +1,32 @@
 package com.zomato.backend.cart;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class CartItems{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+}
+
